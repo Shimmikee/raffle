ce548f9a51ff91af8e39ce739afd7da3b9ff1756
Shimmikee <43670132+Shimmikee@users.noreply.github.com>
2018-10-24 17:26:30 +0800

    delete

--- config.php
+++
@@ -1,8 +0,0 @@
-<?php
-$databaseHost = '172.16.39.241';
-$databaseName = 'eraffle';
-$databaseUsername = 'administrator';
-$databasePassword = 'Agsmc999';
- 
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
-?>
