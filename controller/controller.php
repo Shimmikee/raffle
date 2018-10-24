74b74fd19f9b6a9f8a5557283826b012a92fb227
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-24 17:22:04 +0800

    sorry

---
+++ controller/controller.php
@@ -0,0 +1,16 @@
+<?php
+    function eLogin()
+    {
+        require 'config.php';
+        if(isset($_POST['login']))
+        {
+            $idNumber = mysqli_real_escape_string($db,$_POST['idNumber']);
+            $penName = mysqli_real_escape_string($db,$_POST['penName']);
+            $loginSql = mysqli_query($db,"SELECT * FROM tbl_name WHERE userid = '$idNumber' AND pen_name = '$penName' ");
+            if($loginSql)
+                header('Location:raffle.html');
+            else
+                alert('Invalid userID / pen name');
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include 'controller/controller.php'; ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,7 +14,7 @@
 	<div class="login-card">
 		<h1>Christmas Celebration</h1>
 		<h2>Register now!</h2>
-	
+	<?php eLogin(); ?>
 	<form method="post">
 		<input type="text" name="idNumber" placeholder="ID Number">
 		<input type="text" name="penName" placeholder="Pen Name">
