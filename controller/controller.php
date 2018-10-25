ad05f633ed5f8140fe7b7806471bb0ed166c5778
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-25 09:15:23 +0800

    Update controller/controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -4,9 +4,9 @@
         require 'config.php';
         if(isset($_POST['login']))
         {
-            $idNumber = mysqli_real_escape_string($db,$_POST['idNumber']);
-            $penName = mysqli_real_escape_string($db,$_POST['penName']);
-            $wishList = mysqli_real_escape_string($db,$_POST['wishList']);
+            $idNumber = $_POST['idNumber'];
+            $penName = $_POST['penName'];
+            $wishList = $_POST['wishList'];
             $loginQuery = "INSERT INTO tbl_users(userid,pen_name,wishlist,status) VALUES('$idNumber','$penName','$wishList','0')";
             $loginSql = mysqli_query($db,$loginQuery);
             if($loginSql)
