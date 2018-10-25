9a4d1ebcdfe307ba3ac9b994e7bf2ca4fb3d0fc5
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-25 09:17:54 +0800

    Update controller/controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -7,11 +7,21 @@
             $idNumber = $_POST['idNumber'];
             $penName = $_POST['penName'];
             $wishList = $_POST['wishList'];
-            $loginQuery = "INSERT INTO tbl_users(userid,pen_name,wishlist,status) VALUES('$idNumber','$penName','$wishList','0')";
-            $loginSql = mysqli_query($db,$loginQuery);
-            if($loginSql)
-                echo '<script type="text/javascript">window.alert("DONE")</script>';
+            if(empty($idNumber) && empty($penName) && empty($wishList))
+            {
+                echo '<script type="text/javascript">window.alert("Please fill out all field")</script>';
                 header('Location:index.php');
+            }
+            else
+            {
+                $loginQuery = "INSERT INTO tbl_users(userid,pen_name,wishlist,status) VALUES('$idNumber','$penName','$wishList','0')";
+                $loginSql = mysqli_query($db,$loginQuery);
+                if($loginSql)
+                {
+                    echo '<script type="text/javascript">window.alert("DONE")</script>';
+                    header('Location:index.php');
+                }
+            }
                 
         }
     }
