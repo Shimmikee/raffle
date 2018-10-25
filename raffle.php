7931c83a897b2d2f57103b6ca01aff198ce8399d
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-25 17:34:54 +0800

    Update raffle.php

--- raffle.php
+++ raffle.php
@@ -50,7 +50,7 @@
         <!-- <h1>Dito lalabas, lalabas lang to pag nagselect na ng option</h1> -->
         <?php
             require 'controller/config.php';
-            $Sql = "SELECT userid,pen_name,status,remarks FROM tbl_users WHERE remarks =  null  ORDER BY RAND() ";
+            $Sql = "SELECT userid,pen_name,status,remarks FROM tbl_users ORDER BY RAND() ";
             $quer = mysqli_query($db,$Sql);
             if($row = mysqli_fetch_array($quer))
             {
