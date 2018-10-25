3c408f49761f239ee9a460a8e348aa7910b4f16d
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-25 09:40:42 +0800

    test

--- index.php
+++ index.php
@@ -17,9 +17,11 @@
 	<?php eLogin(); ?>
 	<form method="post">
 		<input type="text" name="idNumber" placeholder="ID Number">
-		<input type="text" name="penName" placeholder="Pen Name">
-		<!-- <input type="text" name="wishList" placeholder="Wishlist"> -->
-		<textarea style="resize:none; font-family: Verdana, sans-serif; padding: 6px; font-size: 16px;" name="wishList" id="" cols="27" rows="10" placeholder="Wishlist (Ex. Item1, Item2, Item3...)"></textarea>
+		<div class="activate">
+			<input type="text" name="penName" placeholder="Pen Name">
+			<!-- <input type="text" name="wishList" placeholder="Wishlist"> -->
+			<textarea style="resize:none; font-family: Verdana, sans-serif; padding: 6px; font-size: 16px;" name="wishList" id="" cols="27" rows="10" placeholder="Wishlist (Ex. Item1, Item2, Item3...)"></textarea>
+		</div>
 		<input type="submit" name="login" class="login login-submit" value="Submit">
 	</form>
 </body>
