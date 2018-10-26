a4aed222ab284983650fb62bbc384af0cc1df4c8
Shimmikee <43670132+Shimmikee@users.noreply.github.com>
2018-10-26 16:55:35 +0800

    update

--- raffle.php
+++ raffle.php
@@ -49,8 +49,51 @@
 				</tr>
 				<tr>
 					<td><button onclick="myFunction()">Monito-Monita</button></td>
-					<td></td>
-					<td></td>
+					<td>
+                        <?php
+                            require 'controller/config.php';
+                            $test = "";
+                            $Sql = "SELECT * FROM tbl_users WHERE nabunot = '$test' AND userid != '{$_SESSION['idNumber']}' ORDER BY RAND()";
+                            //echo '<script type="text/javascript">window.alert('.$_SESSION['idNumber'].')</script>';
+                            $quer = mysqli_query($db,$Sql);
+                            if($row = mysqli_fetch_array($quer))
+                            {
+                                echo '<h1>'.$row['pen_name'].'</h1>';
+                                if($row['monito_status'] == '0')
+                                {
+                                    $upSQL = "UPDATE tbl_users SET nabunot = '{$row['pen_name']}' , hiling = '{$row['wishlist']}', monito_status = '1' WHERE userid = '{$_SESSION['idNumber']}' ";
+                                    $quers = mysqli_query($db,$upSQL);
+                                }
+                                else
+                                {
+                                    echo '<h1>MAY NABUNOT KANA WAG KA PAULIT ULIT !</h1>';
+                                }
+                            }
+                        ?>
+                    </td>
+					<td>
+                        <?php
+                            require 'controller/config.php';
+                            $test = "";
+                            $Sql = "SELECT * FROM tbl_users WHERE nabunot = '$test' AND userid != '{$_SESSION['idNumber']}' ORDER BY RAND()";
+                            //echo '<script type="text/javascript">window.alert('.$_SESSION['idNumber'].')</script>';
+                            $quer = mysqli_query($db,$Sql);
+                            if($row = mysqli_fetch_array($quer))
+                            {
+                                echo '<h1>'.$row['wishlist'].'</h1>';
+                                if($row['monito_status'] == '0')
+                                {
+                                    $upSQL = "UPDATE tbl_users SET nabunot = '{$row['pen_name']}' , hiling = '{$row['wishlist']}', monito_status = '1' WHERE userid = '{$_SESSION['idNumber']}' ";
+                                    $quers = mysqli_query($db,$upSQL);
+                                }
+                                else
+                                {
+                                    echo '<h1>MAY NABUNOT KANA WAG KA PAULIT ULIT !</h1>';
+                                }
+                            }
+                        ?>
+
+                    </td>
 				</tr>
 				<tr>
 					<td><button onclick="myFunction()">Bunutan-2018</button></td>
@@ -59,33 +102,7 @@
 				</tr>
 		</table>
 
-<div id="activate" style="display:none;">
-<div class="raffleResult">
-        <?php
-            
-            require 'controller/config.php';
-            $test = "";
-            $Sql = "SELECT * FROM tbl_users WHERE nabunot = '$test' AND userid != '{$_SESSION['idNumber']}' ORDER BY RAND()";
-            //echo '<script type="text/javascript">window.alert('.$_SESSION['idNumber'].')</script>';
-            $quer = mysqli_query($db,$Sql);
-            if($row = mysqli_fetch_array($quer))
-            {
-                echo '<h1>'.$row['pen_name'].'</h1>';
-                echo '<h1>'.$row['wishlist'].'</h1>';
-                if($row['monito_status'] == '0')
-                {
-                    $upSQL = "UPDATE tbl_users SET nabunot = '{$row['pen_name']}' , hiling = '{$row['wishlist']}', monito_status = '1' WHERE userid = '{$_SESSION['idNumber']}' ";
-                    $quers = mysqli_query($db,$upSQL);
-                }
-                else
-                {
-                    echo '<h1>MAY NABUNOT KANA WAG KA PAULIT ULIT !</h1>';
-                }
-            }
 
-        ?>
-</div>
-</div>
 
 <!-- <script>
     function myFunction(){
