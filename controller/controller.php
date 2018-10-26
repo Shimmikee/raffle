91fdf7fc9561a0207fdb515df10b4c27fbefd06c
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-26 11:03:32 +0800

    Update controller/controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -44,9 +44,10 @@
                 $loginSql = mysqli_query($db,$loginQuery);
                 if($row = mysqli_fetch_array($loginSql))
                 {
-                    echo '<script type="text/javascript">window.alert('.$row['userid'].')</script>';
+                    
                     $_SESSION['idNumber'] = $row['userid'];
                     //header("Location:raffle.php");
+                    echo '<script type="text/javascript">window.alert('.$_SESSION['idNumber'].')</script>';
                 }
                 else
                 {
