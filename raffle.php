bf5af959f06a04e25bbaf6b86e1e3dc57c4de9a6
Shimmikee <43670132+Shimmikee@users.noreply.github.com>
2018-10-26 16:44:53 +0800

    update

--- raffle.php
+++ raffle.php
@@ -41,13 +41,31 @@
 
 <img style="width:1000px;" src="eRaffle/border.png">
 
-<div class="dropdown">
+<!-- <div class="dropdown">
     <button class="dropbtn">Select Option</button>
     <div class="dropdown-content">
       <a href="#"><button onclick="myFunction()">Monito-Monita</button></a>
       <a href="#"><button onclick="myFunction()">Bunutan-2018</button></a>
     </div>
-</div>
+</div> -->
+
+	<table height="400" align="center">
+				<tr>
+					<th style="background-color: #C47451;">SELECT OPTION</th>
+					<th width="200" style="background-color: #C47451;">PEN NAME</th>
+					<th width="400" style="background-color: #C47451;">WISHLIST</th>
+				</tr>
+				<tr>
+					<td><button onclick="myFunction()">Monito-Monita</button></td>
+					<td></td>
+					<td></td>
+				</tr>
+				<tr>
+					<td><button onclick="myFunction()">Bunutan-2018</button></td>
+					<td></td>
+					<td></td>
+				</tr>
+		</table>
 
 <div id="activate" style="display:none;">
 <div class="raffleResult">
@@ -62,15 +80,14 @@
             {
                 echo '<h1>'.$row['pen_name'].'</h1>';
                 echo '<h1>'.$row['wishlist'].'</h1>';
-                if($row['monito_status'] == 0)
+                if($row['monito_status'] == '0')
                 {
                     $upSQL = "UPDATE tbl_users SET nabunot = '{$row['pen_name']}' , hiling = '{$row['wishlist']}', monito_status = '1' WHERE userid = '{$_SESSION['idNumber']}' ";
                     $quers = mysqli_query($db,$upSQL);
                 }
                 else
                 {
-                    echo '<script type="text/javascript">window.alert("MAY NABUNOT KANA WAG KA PAULIT ULIT !")</script>';
-                    //echo '<h1>MAY NABUNOT KANA WAG KA PAULIT ULIT !</h1>';
+                    echo '<h1>MAY NABUNOT KANA WAG KA PAULIT ULIT !</h1>';
                 }
             }
 
