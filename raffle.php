7c2198e79cbd73c5831620580e33ff4b9f5b05da
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-26 13:28:08 +0800

    Update raffle.php

--- raffle.php
+++ raffle.php
@@ -54,8 +54,8 @@
             elogin();
             //session_start();
             $test = "";
-            $Sql = "SELECT userid,pen_name,wishlist,status,remarks FROM tbl_users WHERE userid =  '{$_SESSION['idNumber']}'  ORDER BY RAND() ";
-            echo '<script type="text/javascript">window.alert('.$_SESSION['idNumber'].')</script>';
+            $Sql = "SELECT userid,pen_name,wishlist,status,remarks,nabunot,hiling,monito_status,bunutan_2018 FROM tbl_users WHERE userid =  '{$_SESSION['idNumber']}' AND remarks = '$test'  ORDER BY RAND() ";
+            //echo '<script type="text/javascript">window.alert('.$_SESSION['idNumber'].')</script>';
             $quer = mysqli_query($db,$Sql);
             if($row = mysqli_fetch_array($quer))
             {
