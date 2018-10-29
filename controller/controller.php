397920dddced5341e0b7f8b9525d8910d4dd9a25
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-29 10:12:22 +0800

    test

--- controller/controller.php
+++ controller/controller.php
@@ -83,9 +83,11 @@ GLobal $idNumber;
                 $query_shuffle = mysqli_query($db,$Sql_shuffle);
                 if($row_shuffle = mysqli_fetch_array($query_shuffle))
                 {
+                    echo '<td>PEN NAME : '.$row_shuffle['code_name'].'</td>';
+                    echo '<td>WISHLIST : '.$row_shuffle['wishlist'].'</td>';
                     $updateMonito = mysqli_query($db,"UPDATE tbl_users SET monito_monita = '{$row_shuffle['code_name']}', monito_wishlist = '{$row_shuffle['wishlist']}', monito_status = '1' 
                     WHERE userid = '{$_SESSION['idNumber']}' ");
-                    $updateMonito_remark = mysqli_query($db,"UPDATE tbl_users monito_remark = 'yes' WHERE userid= '{$row_shuffle['userid']}'' ");
+                    $updateMonito_remark = mysqli_query($db,"UPDATE tbl_users monito_remark = 'yes' WHERE userid = '{$row_shuffle['userid']}' ");
                 }
                 else
                 {
