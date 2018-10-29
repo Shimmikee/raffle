4dbc82ed0d4103c6d72bbeddad8f7df2d4115612
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-29 09:26:16 +0800

    Update controller/controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -63,9 +63,32 @@ GLobal $idNumber;
         mysqli_close($db);
     }
 
-    function randUser()
+    function monitoRaffle()
     {
         require 'config.php';
+        if(isset($_POST['btnMonito']))
+        {
+            $monito_status = 0;
+            $monito_remark = "no";
+            $sqlMonito_Status = "SELECT userid,code_name,wishlist,user_stus,monito_monita,monito_wishlist,monito_remark,,monito_status,bunutan,bunutan_wishlist,bunutan_status 
+            FROM tbl_users WHERE userid = '{$_SESSION['idNumber']}'' ";
+            $queryStatus = mysqli_query($db,$sqlMonito_Status);
+            $rowStatus = mysqli_fetch_array($queryStatus);
+            if($rowStatus['monito_status'] == $monito_status)
+            {
+                $Sql_shuffle = "SELECT userid,code_name,wishlist,user_stus,monito_monita,monito_wishlist,monito_remark,,monito_status,bunutan,bunutan_wishlist,bunutan_status 
+                FROM tbl_users WHERE monito_remark = '$monito_remark' AND userid = '{$_SESSION['idNumber']}' ORDER BY RAND() ";
+                $query_shuffle = mysqli_query($db,$Sql_shuffle);
+                if($row_shuffle = mysqli_fetch_array($query_shuffle))
+                {
+                    echo '<script type="text/javascript">window.alert('.$row_shuffle['code_name'].');</script>';
+                }
+            }
+            else
+            {
+                echo '<script type="text/javascript">window.alert("NAKABUNUTO KANA WAG KA PAULIT ULIT");</script>';
+            }
+        }
 
     }
 ?>
