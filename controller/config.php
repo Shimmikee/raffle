ae312f3e9be6090b4ce16c5e7d62c41bb28aeb62
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-29 16:37:36 +0800

    Update controller/config.php

--- controller/config.php
+++ controller/config.php
@@ -1,5 +1,5 @@
 <?php
-    $db = mysqli_connect('172.16.31.39', 'administrator', 'Agsmc999','eraffle');
+    $db = mysqli_connect('172.16.31.39', 'jm', 'jmagumboy28','eraffle');
     if (!$db){
         trigger_error('Could not connect to MySQL: ' . mysqli_connect_error());
     }
