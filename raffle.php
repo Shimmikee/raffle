39df471450a4a7b0e218c34a6373e253f7b37e56
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-29 09:28:39 +0800

    Update raffle.php

--- raffle.php
+++ raffle.php
@@ -40,42 +40,14 @@
 </div>
 
 <img style="width:1000px;" src="eRaffle/border.png">
-
+    <form method="post">
+        <input type="submit" value="Monito - Monita" name="btnMonito"/>
+    </form>
 	<table height="400" align="center">
 				<tr>
-					<th style="background-color: #C47451;">SELECT OPTION</th>
-					<th width="200" style="background-color: #C47451;">PEN NAME</th>
-					<th width="400" style="background-color: #C47451;">WISHLIST</th>
+					<?php monitoRaffle(); ?>
 				</tr>
-				<tr>
-					<td><button onclick="myFunction()">Monito-Monita</button></td>
-					<td>
-                        <script>
-                        function myFunction() {
-                        <?php
-                            require 'controller/config.php';
-                            $test = "";
-                            $Sql = "SELECT * FROM tbl_users WHERE nabunot = '$test' AND userid != '{$_SESSION['idNumber']}' ORDER BY RAND()";
-                            //echo '<script type="text/javascript">window.alert('.$_SESSION['idNumber'].')</script>';
-                            $quer = mysqli_query($db,$Sql);
-                            if($row = mysqli_fetch_array($quer))
-                            {
-                                echo '<h1>'.$row['pen_name'].'</h1>';
-                                if($row['monito_status'] == '0')
-                                {
-                                    $upSQL = "UPDATE tbl_users SET nabunot = '{$row['pen_name']}' , hiling = '{$row['wishlist']}', monito_status = '1' WHERE userid = '{$_SESSION['idNumber']}' ";
-                                    $quers = mysqli_query($db,$upSQL);
-                                }
-                                else
-                                {
-                                    echo '<h1>MAY NABUNOT KANA WAG KA PAULIT ULIT !</h1>';
-                                }
-                            }
-                        ?>
-                        }
-                        </script>
-                    </td>
-					<td>
+					<!-- <td>
                         <script>
                         function myFunction() {
                         <?php
@@ -100,7 +72,7 @@
                         ?>
                         }
                         </script>
-                    </td>
+                    </td> -->
 				</tr>
 				<tr>
 					<td><button onclick="myFunction()">Bunutan-2018</button></td>
