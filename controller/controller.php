c894f3c8027e807065cf93c163b45fe5e7bcca06
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-30 13:45:26 +0800

    Update controller/controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -47,7 +47,11 @@ GLobal $idNumber;
             $idNumber = $_POST['idNumber'];
             $penName = $_POST['penName'];
             $wishList = $_POST['wishList'];
-            if(empty($idNumber) && empty($penName) && empty($wishList))
+            if(empty($idNumber))
+            {
+                echo '<script type="text/javascript">window.alert("ID MO HUY!")</script>';
+            }
+            elseif(empty($penName) && empty($wishList))
             {
                 echo '<script type="text/javascript">window.alert("Please fill out all field")</script>';
             }
