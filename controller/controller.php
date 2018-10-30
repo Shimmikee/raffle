3b41067d1c4a41bb0d2aa4491730f5fb231c6a17
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-10-30 09:32:37 +0800

    Update controller/controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -114,17 +114,27 @@ GLobal $idNumber;
             $rowBunutan = mysqli_fetch_array($bunutan_query);
             if($rowBunutan['bunutan_status'] == $bunutan_status)
             {
+                echo '<script type="text/javascript">window.alert("HEY");</script>';
                 $bunutan_Sql_shuffle = "SELECT userid,code_name,wishlist,user_status,monito_monita,monito_wishlist,monito_remark,monito_status,bunutan,bunutan_wishlist,bunutan_remark,bunutan_status 
                 FROM tbl_users WHERE bunutan_remark = '$bunutan_remark' AND userid != '{$_SESSION['idNumber']}' ORDER BY RAND() ";
                 $bunutan_query_shuffle = mysqli_query($db,$bunutan_Sql_shuffle);
                 if($bunutan_row_shuffle = mysqli_fetch_array($bunutan_query_shuffle))
                 {
+                    echo '<script type="text/javascript">window.alert("HOY);</script>';
                     echo '<td>'.$bunutan_row_shuffle['code_name'].'</td>';
                     echo '<td>'.$bunutan_row_shuffle['wishlist'].'</td>';
                     $updateBunutan = mysqli_query($db,"UPDATE tbl_users SET bunutan = '{$bunutan_row_shuffle['code_name']}', bunutan_wishlist = '{$bunutan_row_shuffle['wishlist']}', bunutan_status = '1' 
                     WHERE userid = '{$_SESSION['idNumber']}' ");
                     $updateBunutan_remark = mysqli_query($db,"UPDATE tbl_users SET bunutan_remark = 'yes' WHERE userid = '{$bunutan_row_shuffle['userid']}' ");
                 }
+                else
+                {
+                    echo '<script type="text/javascript">window.alert("ERROR IN QUERY");</script>';
+                }
+            }
+            else
+            {
+                echo '<script type="text/javascript">window.alert("Sarreh. One bunot only.");</script>';
             }
         }
     }
