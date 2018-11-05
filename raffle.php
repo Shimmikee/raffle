650aa9cefcce97f6e4fd45804bd2d99a8cc3efcb
Jhon Mickelle Agumboy <jhonmickelle28@gmail.com>
2018-11-05 17:44:12 +0800

    Update raffle.php

--- raffle.php
+++ raffle.php
@@ -56,7 +56,7 @@
                         <p>Bunutan 2018 :<br/>
                         <p>Minimum of Php500.00</p>
                     </th>
-                    <?php monitoRaffle(); ?>
+                    <?php echo "TEST"; //monitoRaffle();  ?>
                 </tr>
                 <tr>
                     <th width="150" style="background-color: #EDC9AF;">2nd Week</th>
