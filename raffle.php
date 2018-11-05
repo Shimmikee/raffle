12266cdb7b9a74387e155646accf31ecc4e7c779
Shimmikee <43670132+Shimmikee@users.noreply.github.com>
2018-11-05 16:41:54 +0800

    add reminders

--- raffle.php
+++ raffle.php
@@ -47,11 +47,18 @@
                         <?php monitoRaffle(); ?>
                     <th width="150" style="background-color: #EDC9AF;">Pen Name</th>
                     <th width="200" style="background-color: #EDC9AF; ">Wishlist</th>
+                    <th width="150" style="background-color: #EDC9AF; ">Reminders</th>
                 </tr>
                 <tr>
                     <th width="150" style="background-color: #EDC9AF;">1st Week</th>
                     <th style="border: 2px solid #ffffff;"></th>
                     <th rowspan="5"></th>
+                    <th rowspan="5">
+                        <p>Monito-Monita:<br/>
+                        <p>Minimum of Php100.00<br/>
+                        <p>Bunutan 2018 :<br/>
+                        <p>Minimum of Php500.00</p>
+                    </th>
                 </tr>
                 <tr>
                     <th width="150" style="background-color: #EDC9AF;">2nd Week</th>
