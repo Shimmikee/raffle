749d4db10e67bc6faaadd362a8f08bcaaffe4fe5
Shimmikee <43670132+Shimmikee@users.noreply.github.com>
2018-11-06 13:39:08 +0800

    Update raffle.php

--- raffle.php
+++ raffle.php
@@ -26,7 +26,7 @@
 
 <img style="width:1000px;" src="eRaffle/border.png">
 
-	<table height="400" align="center">
+	<table height="300" align="center">
 				<tr>
                     <td>
                         <?php jemina(); ?>
@@ -47,46 +47,74 @@
 						?>
                         </form>
                     </td>
-                    <th width="150" style="background-color: #EDC9AF; ">Gift Worth</th>
-                    <th width="150" style="background-color: #EDC9AF;">Pen Name</th>
-                    <th width="200" style="background-color: #EDC9AF; ">Wishlist</th>
+                    <th width="300" style="background-color: #EDC9AF; ">Category</th>
+                    <th width="200" style="background-color: #EDC9AF;">Pen Name</th>
                 </tr>
                 <tr>
                     <th width="150" style="background-color: #EDC9AF;">1st Week</th>
-                    <th rowspan="5">
-                        <p>Monito-Monita:<br/>
-                        <p>Minimum of Php100.00<br/>
-                        <p>Bunutan 2018 :<br/>
-                        <p>Minimum of Php500.00</p>
-                    </th>
+                    <th>Something 1</th>
                     <?php jemina(); //monitoRaffle();  ?>
                 </tr>
                 <tr>
                     <th width="150" style="background-color: #EDC9AF;">2nd Week</th>
-                    <th style="border: 2px solid #ffffff;"></th>
+                    <th>Something 2</th>
                 </tr>
                 <tr>
                     <th width="150" style="background-color: #EDC9AF;">3rd Week</th>
-                    <th style="border: 2px solid #ffffff;"></th>
+                    <th>Something 3</th>
                 </tr>
                 <tr>
                     <th width="150" style="background-color: #EDC9AF;">4th Week</th>
-                    <th style="border: 2px solid #ffffff;"></th>
+                    <th>Something 4</th>
+                </tr>
+    </table>
+    
+    <table height="150" align="center">
+				<tr>
+                    <td>
+                        <?php jemina(); ?>
+						<form method="post">
+						<?php
+							// require 'controller/config.php';
+							// //$sql = "SELECT * FROM tbl_users WHERE userid = '{$_SESSION['idNumber']}' ";
+							// $quer = mysqli_query($db,$sql);
+							// $row = mysqli_fetch_array($quer);
+							// if($row['monito_status'] == 0)
+							// {
+							// 	echo '<input style="padding: 10px;" type="submit" value="Click Me" name="btnClickme"/>';
+							// }
+							// else
+							// {
+							// 	echo 'Done'.$row['monito_monita'];
+							// }
+						?>
+                        </form>
+                    </td>
+                    <th width="200" style="background-color: #EDC9AF;">Pen Name</th>
+                    <th width="300" style="background-color: #EDC9AF; ">Wishlist</th>
                 </tr>
                 <tr>
-                    <th width="150" style="background-color: #EDC9AF;">Bunutan 2018</th>
-                    <th style="border: 2px solid #ffffff;"></th>
+                    <th width="150" style="background-color: #EDC9AF;"><input style="padding: 10px;" type="submit" value="Bunutan 2018" name="btnBunutan"/></th>
                 </tr>
-        </table>
+    </table>
+
+    <table height="" align="center">
+				<tr>
+                    <th width="665" style="background-color: #EDC9AF;">Notes</th>
+                </tr>
+                <tr>
+                    <th>
+                        <p>Monito-Monita:<br/>
+                        <p>Minimum of Php100.00<br/>
+                        <p>Bunutan 2018:<br/>
+                        <p>Minimum of Php500.00</p>
+                    </th>
+                </tr>
+    </table>
+
         <form method="get">
         <input style="padding: 10px; margin: 20px auto; display: block;" type="submit" value="Logout" name="btnLogout"/>
         </form>
-
-
-        
-
-
-
 
 </body>
 </html>
